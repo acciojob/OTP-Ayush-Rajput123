8478c8ea82588d3653a5ce7b2320c01a67c83a39
Ayush-Rajput123 <131739732+Ayush-Rajput123@users.noreply.github.com>
2025-08-22 19:43:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+const codes=document.querySelectorAll('.code')
+
+codes.forEach((input, index) => {
+  input.addEventListener("input", () => {
+  input.value = input.value.replace(/\D/g, "")});
+  input.addEventListener('input', () =>{ 
+    if (input.value && index < codes.length - 1) {
+      codes[index + 1].focus();}
+})
+	  input.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace" && !input.value && index > 0) {
+      codes[index - 1].focus();
+    }
+  })
+});
+window.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('.code').focus();
+});
