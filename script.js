9814cb7a83c60111b8ba1c061c369acaa41cb643
Ayush-Rajput123 <131739732+Ayush-Rajput123@users.noreply.github.com>
2025-08-22 20:25:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,42 +1,39 @@
-const codes=document.querySelectorAll('.code')
-
-codes.forEach((input, index) => {
-  input.addEventListener("input", () => {
-  input.value = input.value.replace(/\D/g, "")});
-  input.addEventListener('input', () =>{ 
-    if (input.value && index < codes.length - 1) {
-      codes[index + 1].focus();}
-})
-	  input.addEventListener("keydown", (e) => {
-    if (e.key === "Backspace" && !input.value && index > 0) {
-      codes[index - 1].focus();
-    }
-  })
-});
+const codes = document.querySelectorAll('.code');
+const container = document.querySelector('.code-container');
 window.addEventListener('DOMContentLoaded', () => {
-  document.querySelector('.code').focus();
+  codes[0].focus();
 });
-document.querySelector('.code-container').addEventListener('paste', (e) => {
-  e.preventDefault();
-  const codepaste = e.clipboardData.getData('text');
-  const digitArray = codepaste.replace(/\D/g, '').split('');
-  digitArray.forEach((d, i) => {
-    if (i < codes.length) codes[i].value = d;
-  });
-
-  if (digitArray.length < codes.length) {
-    codes[digitArray.length].focus();
-  } else {
-    codes[codes.length - 1].focus();
-  }
-});
-
 codes.forEach((input, index) => {
-  input.addEventListener("keydown", (e) => {
-    if (e.key === "ArrowLeft" && index > 0) {
-      codes[index - 1].focus();
-    } else if (e.key === "ArrowRight" && index < codes.length - 1) {
+  input.addEventListener('input', () => {
+    input.value = input.value.replace(/\D/g, ''); 
+    if (input.value && index < codes.length - 1) {
       codes[index + 1].focus();
     }
   });
+  input.addEventListener('keydown', (e) => {
+    if (e.key === 'Backspace') {
+      if (input.value) {
+        input.value = '';
+        e.preventDefault();
+      } else if (index > 0) {
+        codes[index - 1].focus();
+        e.preventDefault();
+      }
+    } else if (e.key === 'ArrowLeft' && index > 0) {
+      e.preventDefault();
+      codes[index - 1].focus();
+    } else if (e.key === 'ArrowRight' && index < codes.length - 1) {
+      e.preventDefault();
+      codes[index + 1].focus();
+    }
+  });
+});
+container.addEventListener('paste', (e) => {
+  e.preventDefault();
+  const pastedDigits = e.clipboardData.getData('text').replace(/\D/g, '').slice(0, codes.length);
+  pastedDigits.split('').forEach((digit, i) => {
+    codes[i].value = digit;
+  });
+  const nextFocus = pastedDigits.length < codes.length ? pastedDigits.length : codes.length - 1;
+  codes[nextFocus].focus();
 });
