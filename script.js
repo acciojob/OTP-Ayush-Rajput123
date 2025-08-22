9b9fed6cc3371955bb37391c7f84ed1223e9b9cf
Ayush-Rajput123 <131739732+Ayush-Rajput123@users.noreply.github.com>
2025-08-22 20:04:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ codes.forEach((input, index) => {
     if (input.value && index < codes.length - 1) {
       codes[index + 1].focus();}
 })
-/*	  input.addEventListener("keydown", (e) => {
+	  input.addEventListener("keydown", (e) => {
     if (e.key === "Backspace" && !input.value && index > 0) {
       codes[index - 1].focus();
     }
   })
-});*/
+});
 window.addEventListener('DOMContentLoaded', () => {
   document.querySelector('.code').focus();
 });
